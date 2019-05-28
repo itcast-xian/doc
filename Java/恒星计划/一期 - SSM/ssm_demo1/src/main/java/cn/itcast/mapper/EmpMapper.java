e64e0b3761ad8b0bc1ef71af16c99f4b0b2928c1
dengchangtao <deng.changtao@163.com>
2019-05-28 23:14:08 +0800

    删除文件,重新提交

--- Java/恒星计划/一期 - SSM/ssm_demo1/src/main/java/cn/itcast/mapper/EmpMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.mapper;
-
-import cn.itcast.pojo.Emp;
-
-public interface EmpMapper {
-
-    public Emp findById(Integer id);
-
-}
--- Java/恒星计划/一期 - SSM/ssm_demo1/src/main/java/cn/itcast/service/EmpService.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.service;
-
-import cn.itcast.pojo.Emp;
-
-public interface EmpService {
-
-    public Emp findById(Integer id);
-
-}
